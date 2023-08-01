575b991597cabd4d2229279115d22f29ab855ce6
kahuaski <kahuaski88@gmail.com>
2023-07-31 22:37:38 -0500

    start

--- src/Database_tranqui/bd.config.js
+++ src/Database_tranqui/bd.config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize({
-  dialect:'postgres',
+  dialect:process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
